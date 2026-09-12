38935bf457f1f1491a3495c90df0495d9eb4e17a
Abyl <flwrhx11@gmail.com>
2026-09-13 00:05:44 +0500

    Add Aquarium product and Builder interface

--- src/Aquarium.java
+++ src/Aquarium.java
@@ -1,2 +1,19 @@
 public class Aquarium {
+    private String waterType;
+    private int volumeInLiters;
+    private String lighting;
+    private String fishSpecies;
+
+    Aquarium(String waterType, int volumeInLiters, String lighting, String fishSpecies) {
+        this.waterType = waterType;
+        this.volumeInLiters = volumeInLiters;
+        this.lighting = lighting;
+        this.fishSpecies = fishSpecies;
+    }
+
+    @Override
+    public String toString() {
+        return "Aquarium: " + volumeInLiters + "L " + waterType + " water, " +
+                lighting + " lighting, inhabits " + fishSpecies;
+    }
 }
--- src/AquariumBuilder.java
+++ src/AquariumBuilder.java
@@ -1,2 +1,7 @@
-public class AquariumBuilder {
+public interface AquariumBuilder {
+    AquariumBuilder setWaterType(String waterType);
+    AquariumBuilder setVolume(int volumeInLiters);
+    AquariumBuilder setLighting(String lighting);
+    AquariumBuilder setFishSpecies(String fishSpecies);
+    Aquarium build();
 }
